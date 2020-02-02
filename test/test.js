c9fc60f5036f3e829f3bd3ccc2e2e8efa95acf86
Jaid <jaid.jsx@gmail.com>
2020-02-02 20:21:18 +0100

    Fixed test
    Signed-off-by: Jaid <jaid.jsx@gmail.com>

--- test/test.js
+++ test/test.js
@@ -9,6 +9,6 @@ const {default: pxByDefault} = indexModule
 
 it("should run", () => {
   expect(pxByDefault(30)).toBe("30px")
-  expect(pxByDefault("3 rem")).toBe("3 rem")
-  expect(pxByDefault(" 100 vw ")).toBe(" 100 vw ")
+  expect(pxByDefault("3rem")).toBe("3rem")
+  expect(pxByDefault(" 100vw ")).toBe(" 100vw ")
 })
